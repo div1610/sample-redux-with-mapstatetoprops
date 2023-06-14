4a5ff312abe67d318d70e170111b5a2cbcc9f72e
divyesh <dgajera496@gmail.com>
2023-06-14 17:45:55 +0530

    all project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PostsContainer from './components/PostsContainer'
+
+function App() {
+  return (
+    <div className='App'>
+      <PostsContainer />
+    </div>
+  );
+}
+
+export default App;
